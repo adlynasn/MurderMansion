532a65f95165e6ac83eea46ab651e42c38c9d4e3
adlynasn <nuradlina230302@gmail.com>
2023-06-11 20:36:23 +0800

    added alternative algorithm to part 7 solution

---
+++ .idea/MAIN/part7(2).py
@@ -0,0 +1,44 @@
+from itertools import permutations
+
+# Function to check if a word exists in a given word list
+def is_word(word, word_list):
+    return word in word_list
+
+# Function to find valid words from a list of jumbled letters
+def find_valid_words(letters, word_list):
+    valid_words = []
+    # Generate all permutations of the letters
+    permutations_list = [''.join(perm) for perm in permutations(letters)]
+    # Check each permutation if it is a valid word
+    for word in permutations_list:
+        if is_word(word, word_list):
+            valid_words.append(word)
+    return valid_words
+
+# Function to solve the jumbled word problem and retrieve the secret message using brute-force
+def solve_secret_message_brute_force(jumbled_words, word_list):
+    secret_message = []
+    for line in jumbled_words:
+        valid_words = []
+        # Try all possible permutations of the line
+        for perm in permutations(line):
+            word = ''.join(perm)
+            if is_word(word, word_list):
+                valid_words.append(word)
+        if len(valid_words) == 1:
+            secret_message.append(valid_words[0])
+        else:
+            secret_message.append('')  # Empty string if no valid word found
+    return ' '.join(secret_message)
+
+# Example usage
+jumbled_words = ['haTt', 'enPros', 'asH', 'eMvito']
+word_list_file = 'word_list.txt'
+
+# Read the word list from the text file and store in a list
+with open(word_list_file, 'r') as file:
+    word_list = [line.strip() for line in file]
+
+# Call the function to solve the problem and retrieve the secret message using brute-force
+secret_message = solve_secret_message_brute_force(jumbled_words, word_list)
+print("Secret Message (Brute-Force):", secret_message)
