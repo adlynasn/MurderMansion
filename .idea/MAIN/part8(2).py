ada73de6a591ad834b8aa7d191d8d3de5c440c89
faiqahAzman <u2001400@siswa.um.edu.my>
2023-06-12 12:21:15 +0800

    update

--- .idea/MAIN/part8(2).py
+++ .idea/MAIN/part8(2).py
@@ -1,34 +1,36 @@
-class WeightedDecisionMatrix:
-    def __init__(self):
-        self.data = []
-
-    def add_person(self, net_worth, character_trait, relationship):
-        self.data.append([net_worth, character_trait, relationship])
-
-    def get_person_scores(self):
-        return [sum(row) for row in self.data]
+def calculate_weighted_decision_matrix(matrix, weights):
+    scores = []
+    for row in matrix:
+        weighted_sum = sum(val * weight for val, weight in zip(row, weights))
+        scores.append(weighted_sum)
+    return scores
 
 
-def determine_highest_motive(matrix):
-    scores = matrix.get_person_scores()
-    highest_motive_index = scores.index(max(scores))
-    return highest_motive_index
+# Criteria weights (adjust as needed)
+net_worth_weight = 0.4
+relationship_weight = 0.1
+character_trait_weight = 0.3
 
+# Person data
+persons = ['Jones', 'Jenna', 'Peter', 'Penelope', 'Will']
 
-# Create a weighted decision matrix
-decision_matrix = WeightedDecisionMatrix()
+# Individual net worth, relationship, and character trait values
+net_worth_values = [1000000, 700000, 50000, 500000, 10000]
+relationship_values = [0.7, 0.8, 0.5, 0.5, 0.3]
+character_trait_values = [0.05, 0.5, 0.5, 0.7, 0.4]
 
-# Add persons with their scores for each criterion
-decision_matrix.add_person(0.8, 0.7, 0.8)
-decision_matrix.add_person(0.6, 0.6, 0.5)
-decision_matrix.add_person(0.9, 0.4, 0.3)
-decision_matrix.add_person(0.7, 0.5, 0.6)
-decision_matrix.add_person(0.6, 0.8, 0.4)
+# Decision matrix with net worth, relationship, and character trait as columns
+decision_matrix = [
+    net_worth_values,
+    relationship_values,
+    character_trait_values
+]
 
-# Determine the person with the highest motive
-highest_motive_index = determine_highest_motive(decision_matrix)
+# Calculate weighted scores for each person
+scores = calculate_weighted_decision_matrix(decision_matrix, [net_worth_weight, relationship_weight, character_trait_weight])
 
-# Print the person with the highest motive
-persons = ["Person A", "Person B", "Person C", "Person D", "Person E"]
-highest_motive_person = persons[highest_motive_index]
-print("The person with the highest motive is:", highest_motive_person)
+# Find the person with the highest total score
+person_with_highest_motive = persons[scores.index(max(scores))]
+
+# Print the results
+print("Person with the highest motive:", person_with_highest_motive)
