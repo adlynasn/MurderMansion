5daee375b45dd58e279f154b4b7d6ef3f2d5ad09
adlynasn <nuradlina230302@gmail.com>
2023-06-21 16:57:36 +0800

    edited part1 node code

--- .idea/MAIN/part1.py
+++ .idea/MAIN/part1.py
@@ -2,10 +2,9 @@ from collections import deque
 
 
 class Node:
-    def _init_(self, name):
+    def __init__(self, name):
         self.name = name
         self.children = []
-
 
     def add_child(self, child):
         self.children.append(child)
@@ -16,16 +15,13 @@ def bfs(root):
     queue = deque()
     queue.append(root)
 
-
     print("*** Start searching for clues *** ")
     print("")
     while queue:
         node = queue.popleft()
         print(node.name)  # You can modify this to perform any operation on the node
 
-
         visited.add(node)
-
 
         for child in node.children:
             if child not in visited:
@@ -53,7 +49,6 @@ upper_Balcony2 = Node("Upper Balcony 2")
 master_Bedroom = Node("Master Bedroom")
 Upper_Utility = Node("Upper Utility")
 
-
 entrance.add_child(main_hall)
 main_hall.add_child(utility)
 main_hall.add_child(dining)
@@ -71,6 +66,4 @@ room2.add_child(upper_Balcony2)
 room2.add_child(master_Bedroom)
 master_Bedroom.add_child(Upper_Utility)
 
-
-# Perform BFS starting from the Mansion node
 bfs(entrance)
